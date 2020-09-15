0251519bb2a9ef58c359e9adaa065d2c29254066
jasoniel <jazoniel@gmail.com>
2020-09-15 20:12:47 -0300

    Chapter 4 - Creating the Data Model

--- ServerApp/Startup.cs
+++ ServerApp/Startup.cs
@@ -6,9 +6,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ServerApp.Models;
 
 namespace ServerApp
 {
@@ -24,11 +26,13 @@ namespace ServerApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider services)
         {
             if (env.IsDevelopment())
             {
@@ -55,7 +59,7 @@ namespace ServerApp
             });
 
             app.UseSpa(spa =>
-           {
+            {
                string strategy = Configuration
                     .GetValue<String>("DevTools:ConnectionStrategy");
 
@@ -67,8 +71,10 @@ namespace ServerApp
                    spa.Options.SourcePath = "../ClientApp";
                    spa.UseAngularCliServer("start");
                }
+            });
 
-           });
+            SeedData.SeedDatabase(services.GetRequiredService<DataContext>());
+
         }
     }
 }
